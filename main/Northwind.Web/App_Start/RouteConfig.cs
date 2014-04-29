b67cfb373ca30a15fb52240371b60279c71d2ec4
lelong37 <lelong37@gmail.com>
2014-04-29 18:09:24 -0500

    Change default RouteConfig to navigate straight to SPA url, redployed live demo site.

--- main/Northwind.Web/App_Start/RouteConfig.cs
+++ main/Northwind.Web/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace Northwind.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Default", "{controller}/{action}/{id}", new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new {controller = "Home", action = "spa", id = UrlParameter.Optional}
                 );
         }
     }
